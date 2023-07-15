1a89f1da08981eea1707357df5ced528af72705e
Samad Saleem <samadsaleem1998@gmail.com>
2023-07-15 22:28:54 +0500

    App Structure settled

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,23 @@
-import {
- Text,
- View,
-} from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
-function HomeScreen() {
+const HomeScreen = () => {
  return (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-   <Text>This is the Home Screen</Text>
+  <View style={styles.container}>
+   <Text style={styles.text}>This is the Home Screen</Text>
   </View>
  );
-}
+};
+
+const styles = StyleSheet.create({
+ container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+ text: {
+  fontSize: 18,
+ },
+});
 
 export default HomeScreen;
