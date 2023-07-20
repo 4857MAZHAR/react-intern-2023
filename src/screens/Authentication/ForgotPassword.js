6c4e036093e2d62b21117d4368d5326b599d2c7b
Samad Saleem <samadsaleem1998@gmail.com>
2023-07-20 15:18:43 +0500

    Re: Code Refactor

--- src/screens/Authentication/ForgotPassword.js
+++ src/screens/Authentication/ForgotPassword.js
@@ -1,14 +1,19 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+//React
 import React from 'react';
-import TextInput from "../../components/TextInput";
-import BackArrowIcon from "../../components/Icons/BackArrowIcon";
+import { KeyboardAvoidingView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+//HookForm
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+//Comps
+import TextInput from "../../components/TextInput";
+import TextButton from "../../components/TextButton";
+import BackArrowIcon from "../../components/Icons/BackArrowIcon";
+//Utils
 import { SCREEN_NAME } from "../../utils/Const";
 import { COLORS } from "../../utils";
-import TextButton from "../../components/TextButton";
 
+//Validation using Yup
 const validationSchema = yup.object().shape({
  email: yup.string().email('Invalid email').required('You have not entered an email address'),
 });
@@ -25,43 +30,51 @@ const ForgotPassword = ({ navigation }) => {
   console.log(data);
  };
  return (
-  <ScrollView contentContainerStyle={styles.container}>
-   <TouchableOpacity style={styles.arrowButton} onPress={handleBackButtonPress}>
-    <BackArrowIcon width={30} height={30} />
-   </TouchableOpacity>
+  <KeyboardAvoidingView
+   style={styles.containerMain}
+   behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+   keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+  >
+   <ScrollView contentContainerStyle={styles.container}>
+    <TouchableOpacity style={styles.arrowButton} onPress={handleBackButtonPress}>
+     <BackArrowIcon width={30} height={30} />
+    </TouchableOpacity>
 
-   <Text style={styles.title}>Forgot Password</Text>
-   <Text style={styles.description}>Pease enter your email address below{"\n"} and we will send you a link to reset{"\n"} a password </Text>
+    <Text style={styles.title}>Forgot Password</Text>
+    <Text style={styles.description}>Pease enter your email address below{"\n"} and we will send you a link to reset{"\n"} a password </Text>
 
-   <View style={styles.inputContainer}>
-    <Controller
-     control={control}
-     name="email"
-     defaultValue=""
-     render={({ field }) => (
-      <TextInput
-       placeholder="Email address"
-       onChangeText={field.onChange}
-       value={field.value}
-       error={errors.email?.message}
-      />
-     )}
-    />
-   </View>
+    <View style={styles.inputContainer}>
+     <Controller
+      control={control}
+      name="email"
+      defaultValue=""
+      render={({ field }) => (
+       <TextInput
+        placeholder="Email address"
+        onChangeText={field.onChange}
+        value={field.value}
+        error={errors.email?.message}
+       />
+      )}
+     />
+    </View>
 
-   <TextButton text="Reset Password" isSignInButton onPress={handleSubmit(onSubmit)} />
-  </ScrollView>
+    <TextButton text="Reset Password" isSignInButton onPress={handleSubmit(onSubmit)} />
+   </ScrollView>
+  </KeyboardAvoidingView>
  );
 };
 
 export default ForgotPassword;
 
 const styles = StyleSheet.create({
- container: {
+ containerMain: {
   flexGrow: 1,
-  backgroundColor: COLORS.white,
-  padding: 20,
+  backgroundColor: COLORS.primaryBackgroundColor,
+ },
+ container: {
   alignItems: 'center',
+  padding: 15,
  },
  arrowButton: {
   alignSelf: "flex-start"
@@ -75,7 +88,8 @@ const styles = StyleSheet.create({
  description: {
   marginTop: 30,
   fontSize: 18,
-  textAlign: "center"
+  textAlign: "center",
+  color: COLORS.black
  },
  inputContainer: {
   width: '100%',
--- src/screens/Authentication/Login.js
+++ src/screens/Authentication/Login.js
@@ -1,21 +1,27 @@
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+//React
 import React from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+//HookForm and yup
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { useForm, Controller } from 'react-hook-form';
+//comps
 import TextInput from "../../components/TextInput";
 import TextButton from "../../components/TextButton";
 import IconButton from "../../components/IconButton";
 import CrossIcon from "../../components/Icons/CrossIcon";
+//utils
 import { SCREEN_NAME } from "../../utils/Const";
 import { COLORS } from "../../utils";
 
 
-
+//Validation using Yup
 const validationSchema = yup.object().shape({
  email: yup.string().email('Invalid email').required('You have not entered an email address'),
  password: yup.string().required('You have not entered a password'),
 });
+
+
 const Login = ({ navigation }) => {
  const { handleSubmit, control, formState: { errors } } = useForm({
   resolver: yupResolver(validationSchema),
@@ -36,55 +42,63 @@ const Login = ({ navigation }) => {
  };
 
  return (
-  <ScrollView contentContainerStyle={styles.container}>
-   <TouchableOpacity style={styles.crossIcon} onPress={handleCrossIcon} >
-    <CrossIcon width={30} height={30} />
-   </TouchableOpacity>
-   <Text style={styles.lineText}>
-    Sign in to BOXD
-   </Text>
+  <KeyboardAvoidingView
+   style={styles.containerMain}
+   behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+   keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+  >
+   <ScrollView contentContainerStyle={styles.container}>
+    <TouchableOpacity style={styles.crossIcon} onPress={handleCrossIcon} >
+     <CrossIcon width={30} height={30} />
+    </TouchableOpacity>
+    <Text style={styles.lineText}>
+     Sign in to BOXD
+    </Text>
 
-   <View style={styles.inputContainer}>
-    <Controller
-     control={control}
-     name="email"
-     defaultValue=""
-     render={({ field }) => (
-      <TextInput
-       placeholder="Email"
-       onChangeText={field.onChange}
-       value={field.value}
-       error={errors.email?.message}
-      />
-     )}
-    />
-    <Controller
-     control={control}
-     name="password"
-     defaultValue=""
-     render={({ field }) => (
-      <TextInput
-       placeholder="Password"
-       onChangeText={field.onChange}
-       value={field.value}
-       secureTextEntry={true}
-       error={errors.password?.message}
-      />
-     )}
-    />
-   </View>
-   <TextButton text="Sign in" isSignInButton onPress={handleSubmit(onSubmit)} />
-   <IconButton text="Sign in with Google" onPress={handleGoogleSignIn} />
-   <TextButton text="Forgotten your password" onPress={handleForgotPress} />
-  </ScrollView>
+    <View style={styles.inputContainer}>
+     <Controller
+      control={control}
+      name="email"
+      defaultValue=""
+      render={({ field }) => (
+       <TextInput
+        placeholder="Email"
+        onChangeText={field.onChange}
+        value={field.value}
+        error={errors.email?.message}
+       />
+      )}
+     />
+     <Controller
+      control={control}
+      name="password"
+      defaultValue=""
+      render={({ field }) => (
+       <TextInput
+        placeholder="Password"
+        onChangeText={field.onChange}
+        value={field.value}
+        secureTextEntry={true}
+        error={errors.password?.message}
+       />
+      )}
+     />
+    </View>
+    <TextButton text="Sign in" isSignInButton onPress={handleSubmit(onSubmit)} />
+    <IconButton text="Sign in with Google" onPress={handleGoogleSignIn} />
+    <TextButton text="Forgotten your password" onPress={handleForgotPress} />
+   </ScrollView>
+  </KeyboardAvoidingView>
  );
 };
 
 const styles = StyleSheet.create({
- container: {
+ containerMain: {
   flexGrow: 1,
-  alignItems: 'center',
   backgroundColor: COLORS.primaryBackgroundColor,
+ },
+ container: {
+  alignItems: 'center',
   padding: 15,
  },
  crossIcon: {
