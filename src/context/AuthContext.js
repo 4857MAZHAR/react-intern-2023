945c575a3bd30347852237444a071fcd67b1e7f0
Nabiya Fatima <n.f.quddus@gmail.com>
2023-07-22 12:55:32 -0700

    code refactor

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,2 +1,25 @@
-import React from "react";
-const AuthContext=createC
+import React,{createContext,useState} from "react";
+export const AuthContext=createContext();
+
+export  const AuthProvider=({children})=>{
+
+const [isLoading,setIsLoading]=useState(false);
+const [userInfo,setUserInfor]=useState(null);
+const [isLoggedIn,setIsLoggedIn]=useState(null);
+
+const login=(email,password)=>{
+setIsLoggedIn(true);
+}
+
+
+const register=(email,password)=>{
+}
+  return(
+    <AuthContext.Provider value={{register,login,isLoggedIn}}>{children}</AuthContext.Provider>
+    )
+
+};
+
+
+
+
