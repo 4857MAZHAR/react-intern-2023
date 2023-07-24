8eb5c049555e80691cc9b23705ee788aca176b03
Samad Saleem <samadsaleem1998@gmail.com>
2023-07-24 17:20:52 +0500

    Dashboard Settled

--- src/screens/BottomScreens/DashboardScreen.js
+++ src/screens/BottomScreens/DashboardScreen.js
@@ -1,17 +1,24 @@
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View, FlatList } from 'react-native';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from "../../components/Header";
 import { colors } from "../../utils/theme/colors/colors";
-import { Image } from "react-native";
 import BellIcon from "../../assets/icons/BellIcon";
 import { Text } from "react-native";
 import LogoIcon from "../../assets/icons/LogoIcon";
 
-const Dashboard = () => {
+//components
+import TodoCard from '../../components/TodoCard';
+import GuideCard from '../../components/GuideCard';
+import BodyText from '../../components/Typography/BodyText';
+import Heading from '../../components/Typography/Heading';
+
+const DashboardScreen = () => {
  const userName = "28&Moon";
  const notificationCount = 3;
  return (
   <SafeAreaView>
+
    <Header
     title={`Hello ${userName}`}
     rightComponent={
@@ -30,11 +37,40 @@ const Dashboard = () => {
      </View>
     }
    />
+
+   <Heading
+    textstyle={styles.welcometext}
+    type="h1"
+    text={'Welcome to your \nBOXD 2 week trial'}
+   />
+   <GuideCard
+    message1={"it's nice to have u with us!"}
+    message2={
+     'Reach out to us directly for support,\nany questions or feedback.'
+    }
+   />
+   <View style={styles.sheet}>
+    <View style={styles.horizontalflex}>
+     <Heading type="h4" textstyle={styles.headtextstyle1} text={'To do'} />
+     <BodyText
+      type="rg"
+      textstyle={styles.numtodotextstyle}
+      text={`${todo?.length} of 5`}
+     />
+    </View>
+    <FlatList
+     showsVerticalScrollIndicator={false}
+     contentContainerStyle={styles.flatlisttodo}
+     data={todo}
+     renderItem={({ item }) => <TodoCard item={item} />}
+     keyExtractor={item => item.id}
+    />
+   </View>
   </SafeAreaView>
  );
 };
 
-export default Dashboard;
+export default DashboardScreen;
 
 const styles = StyleSheet.create({
  rightContainer: {
@@ -64,5 +100,45 @@ const styles = StyleSheet.create({
  countText: {
   color: colors.white,
   fontSize: 11
- }
+ },
+
+ sheet: {
+  height: 500,
+  width: '100%',
+  backgroundColor: colors.lightGray1,
+  marginTop: 20,
+  borderTopRightRadius: 35,
+  borderTopLeftRadius: 35,
+  paddingVertical: 20,
+  paddingHorizontal: 25,
+ },
+ headtextstyle1: {
+  color: colors.textColor,
+ },
+ numtodotextstyle: {
+  color: colors.textColor,
+ },
+ welcometext: {
+  textAlign: 'left',
+  marginTop: 25,
+  marginBottom: 10,
+  paddingLeft: 13,
+  color: colors.greenDark1,
+ },
+
+ horizontalflex: {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  marginBottom: 15,
+ },
+ centerflex: {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+ },
+ flatlisttodo: {
+  paddingBottom: 30,
+ },
 });
